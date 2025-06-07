c29b33301e4e1517f8e7c85d4426df89fdc8842a
benyoussefmouad1 <benyoussefmouad1@gmail.com>
2025-06-07 14:55:36 +0100

    ..

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-<<<<<<< HEAD
     react(),
-=======
     react()
->>>>>>> fd533fe134ad52b1ca2e20f19ae4dcc2b2edfdc6
   ],
   
 })
